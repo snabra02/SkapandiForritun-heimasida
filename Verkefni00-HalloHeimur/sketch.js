18be9e8d6f6081ab5ab582320565403ea0eb9066
snabra02 <innerstar100@gmail.com>
2021-04-07 14:18:33 +0000

    hi

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -1,12 +1,22 @@
 // Hér kemur kóðinn þinn:
 
 function setup() {
-  createCanvas(400, 400);
-  background(255,200,0);
+  createCanvas(7000, 1000);
+  background(199, 255, 247);
   frameRate(4);
 }
 
 function draw() {
-  fill(random(0,255));
+  fill(217, 48, 48);
   ellipse(200,200,50,50);
+  fill(217, 48, 48);
+  ellipse(200,500,50,50);
+  fill(217, 48, 48);
+  ellipse(200,700,50,50);
+  fill(217, 48, 48);
+  square(200,900,50,50);
+  fill(217, 48, 48);
+  square(200,1000,50,50);
+  fill(217, 48, 48);
+  square(200,1200,50,50);
 }
---
+++ Verkefni01/sketch.js
@@ -0,0 +1,12 @@
+// Hér kemur kóðinn þinn:
+
+function setup() {
+  createCanvas(1000, 1000);
+  background(199, 255, 247);
+  frameRate(4);
+}
+
+function draw() {
+  fill(random(0,255));
+  ellipse(200,200,50,50);
+}
