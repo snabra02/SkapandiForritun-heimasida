5c49927b1c0612f5b537f197ae9674a305cae581
snabra02 <innerstar100@gmail.com>
2021-04-15 16:32:34 +0000

    up

--- Verkefni05/sketch.js
+++ Verkefni05/sketch.js
@@ -1,7 +1,5 @@
-// Hér kemur kóðinn þinn:
-
 function setup() {
-  createCanvas(200,300);
+  createCanvas(3000,1000);
   background(255, 215, 0);
   fill(0);
   colorMode(HSB);
@@ -10,7 +8,9 @@ function setup() {
 
 function draw(){
   fill(random(0,360),75,100);
-  for(var i = 0; i < 5 ; i = i + 1) {
-    ellipse(100, 50 + i*20, 15, 15);
+  for(var i = 0; i < 20 ; i = i + 1) {
+    ellipse(random, 50 + i*20, 15, 15);
+
+
   }
 }
